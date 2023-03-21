923d9e5439b4ad85bf97aef347672b6193f31fc3
Karim Rachidi <karim.m.rachidi@gmail.com>
2023-03-21 15:15:20 +0100

    PROSPR-1 Creación del proyecto

--- src/main/java/com/eoi/ProyectoSpring/ProyectoSpringApplication.java
+++ src/main/java/com/eoi/ProyectoSpring/ProyectoSpringApplication.java
@@ -1,13 +1,38 @@
 package com.eoi.ProyectoSpring;
 
+import com.eoi.ProyectoSpring.entities.Customer;
+import com.eoi.ProyectoSpring.repositories.CustomerRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @SpringBootApplication
-public class ProyectoSpringApplication {
+public class ProyectoSpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringApplication.class, args);
 	}
 
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Customer customer = new Customer("Pepe", "Lopez", "Alegria",
+				LocalDate.of(1982, 5, 9),
+				LocalDateTime.now());
+
+
+		customerRepository.save(customer);
+	}
 }
--- src/test/java/com/eoi/ProyectoSpring/ProyectoSpringApplicationTests.java
+++ src/test/java/com/eoi/ProyectoSpring/ProyectoSpringApplicationTests.java
@@ -1,13 +1,34 @@
 package com.eoi.ProyectoSpring;
 
+import com.eoi.ProyectoSpring.entities.Customer;
+import com.eoi.ProyectoSpring.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @SpringBootTest
-class ProyectoSpringApplicationTests {
+class ProyectoSpringApplicationTests implements CommandLineRunner {
+
+	@Autowired
+	CustomerRepository customerRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Customer customer = new Customer("Pepe", "Lopez","Alegria",
+				LocalDate.of(1982,5,9),
+				LocalDateTime.now());
+
+
+		customerRepository.save(customer);
+
+	}
 }
